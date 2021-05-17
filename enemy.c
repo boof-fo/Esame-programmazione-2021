a2f13374ec4d58892727bf2f32e4ad6ead1ec956
Capibarbas <lucabarbieri903@gmail.com>
2021-05-18 00:01:19 +0200

    nemici

--- enemy.c
+++ enemy.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-typedef int bool;
-#define true 1
-#define false 0
-
 #include "enemy.h"
+
+void setEnemyID(enemy *enemy, int ID)
+{
+	enemy->ID = ID;
+}
+
+void setEnemyHP(enemy *enemy, int HP)
+{
+	enemy->HP = HP;
+}
+
+void setEnemyDamage(enemy *enemy, int damage)
+{
+	enemy->damage = damage;
+}
+
+void setEnemyAbility(enemy *enemy, int abilityID)
+{
+	enemy->specialAbility = abilityID;
+}
+
+void spawnEnemy(enemy *enemy, int ID, int HP, int damage,int abilityID)
+{
+	setEnemyID(enemy, ID);
+	setEnemyHP(enemy, HP);
+	setEnemyDamage(enemy, damage);
+	setEnemyAbility(enemy, abilityID);
+}
--- enemy.h
+++ enemy.h
@@ -1,10 +1,7 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-
+//struttura di un nemico
 typedef struct{
 	//tipo di nemico
 	int ID;
@@ -12,8 +9,19 @@ typedef struct{
 	int HP;
 	//punti danno del nemico
 	int damage;
-	//TODO: non so
+	//TODO: medico si cura da solo, 
 	int specialAbility;
+	//descrizione nemico
+	char description[120];
 }enemy;
+
+void setEnemyHP(enemy *enemy, int HP);
+void setEnemyDamage(enemy *enemy, int damage);
+void setEnemyAbility(enemy *enemy, int abilityID);
+void setEnemyID(enemy *enemy, int ID);
+
+//crea il nemico
+void spawnEnemy(enemy *enemy, int ID, int HP, int damage,int abilityID);
+
 
 #endif
